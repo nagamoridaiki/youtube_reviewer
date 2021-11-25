5f72f2489eb6d2ee5436f5cce754632de6368e42
nagamoridaiki <user@USERnoMacBook-puro.local>
2021-11-25 16:10:19 +0900

    画面遷移まで

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = (api) => {
+    const isProduction = api.env('production');
+    api.cache(true);
+    const presets = [
+      [
+        '@babel/preset-env',
+        {
+            targets: {
+                chrome: '79',
+            },
+            useBuiltIns: 'entry',
+            corejs: 3,
+        },
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: !isProduction,
+        },
+      ],
+    ];
+  
+    const plugins = [];
+  
+    return {
+      presets,
+      plugins,
+    };
+  };
+  
