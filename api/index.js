55db80f89077e1343ef3c1ede467e90fed54ce30
nagamoridaiki <user@USERnoMacBook-puro.local>
2021-12-02 19:52:14 +0900

    お気に入りの登録・読み取り・削除API

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { google } = require('googleapis');
+const { readFavoriteIds, writeFavoriteIds } = require('../utils/favorite');
 
 /* 先ほど取得したAPIキーを設定する */
-const YOUTUBE_API_KEY = 'AIzaSyAnjSGIIsXbSNU3kqoIJQkfWrosP4WpPqg';
+const YOUTUBE_API_KEY = 'XXXXXXXXXXXXXX';
 
 const youtube = google.youtube({
   version: 'v3',
@@ -32,6 +33,25 @@ router.get('/videos/search/:keyword', (req, res, next) => {
     res.json({ items, nextPageToken });
   })().catch(next);
 
+});
+
+// お気に入り動画の取得
+router.get('/videos/favorites', (req, res, next) => {
+  (async () => {
+    // お気に入り動画IDを取得
+    const favoriteIds = await readFavoriteIds();
+    if (!favoriteIds.length) {
+      // お気に入りが1つもなければ空配列を返す
+      res.json({ items: [] });
+      return;
+    }
+    // お気に入りのIDから動画の取得
+    const { data: { items } } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: favoriteIds.join(','),
+    });
+    res.json({ items });
+  })().catch(next);
 });
 
 router.get('/videos/:videoId', (req, res, next) => {
@@ -68,5 +88,46 @@ router.get('/videos/:videoId/related', (req, res, next) => {
     res.json({ items, nextPageToken });
   })().catch(next);
 });
+
+// お気に入り動画ID一覧取得
+router.get('/favorites', (req, res, next) => {
+  readFavoriteIds().then((data) => {
+    res.json(data);
+  }).catch(next);
+});
+
+// お気に入り登録・解除
+router.route('/favorites/:id')
+  // お気に入り登録
+  .post((req, res, next) => {
+    (async () => {
+      const { id } = req.params;
+      // 現時点のお気に入りリストを読み込み
+      const favoriteIds = await readFavoriteIds();
+      if (favoriteIds.indexOf(id) === -1) {
+        // パラメータに指定されたIDが現時点のお気に入りになかった場合
+        // お気に入りリストに追加
+        favoriteIds.unshift(id);
+        // お気に入りリストを書き込む
+        writeFavoriteIds(favoriteIds);
+      }
+      res.end();
+    })().catch(next);
+  })
+  // お気に入り解除
+  .delete((req, res, next) => {
+    (async () => {
+      const { id } = req.params;
+      // 現時点のお気に入りリストを読み込み
+      const favoriteIds = await readFavoriteIds();
+      const indexOfId = favoriteIds.indexOf(id);
+      if (indexOfId !== -1) {
+        // パラメータに指定されたIDが現時点のお気に入りにあった場合
+        // 指定されたIDを削除したものをお気に入りリストに書き込む
+        writeFavoriteIds(favoriteIds.filter((favoriteId) => (favoriteId !== id)));
+      }
+      res.end();
+    })().catch(next);
+  });
 
 module.exports = router;
