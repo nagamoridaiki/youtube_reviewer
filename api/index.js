bf388c942160ca80503157ee1c95f361c9cb2cf1
nagamoridaiki <user@USERnoMacBook-puro.local>
2021-12-02 19:16:09 +0900

    動画一覧から再生と関連動画ページまで実装

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { google } = require('googleapis');
 
 /* 先ほど取得したAPIキーを設定する */
-const YOUTUBE_API_KEY = 'XXXXXXXXXXXXXXX';
+const YOUTUBE_API_KEY = 'AIzaSyAnjSGIIsXbSNU3kqoIJQkfWrosP4WpPqg';
 
 const youtube = google.youtube({
   version: 'v3',
@@ -32,6 +32,41 @@ router.get('/videos/search/:keyword', (req, res, next) => {
     res.json({ items, nextPageToken });
   })().catch(next);
 
+});
+
+router.get('/videos/:videoId', (req, res, next) => {
+  const { videoId } = req.params;
+  (async () => {
+    // 動画の情報を取得
+    const { data: { items } } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: videoId,
+    });
+    res.json(items[0]);
+  })().catch(next);
+});
+
+// 関連動画取得
+router.get('/videos/:videoId/related', (req, res, next) => {
+  const { videoId: relatedToVideoId } = req.params;
+  const { pageToken } = req.query;
+  (async () => {
+    // 関連動画のIDを取得
+    const { data: { items: idItems, nextPageToken } } = await youtube.search.list({
+      part: 'id',
+      relatedToVideoId,
+      type: 'video',
+      maxResults: 20,
+      pageToken,
+    });
+    // 動画の情報を取得
+    const ids = idItems.map(({ id: { videoId } }) => videoId);
+    const { data: { items } } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: ids.join(','),
+    });
+    res.json({ items, nextPageToken });
+  })().catch(next);
 });
 
 module.exports = router;
